8df0ecc2e4eef09e2f7d9fea24d462a21b888c2a
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2018-03-09 23:30:31 -0600

    added an example disabled section

--- seatmap/src/app/app.component.ts
+++ seatmap/src/app/app.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 export class Seat {
 	id: number;
-	section: string;
+	section_id: string;
 	selected: boolean; // image switch
 	sold: boolean; // disable the control
 	ha: boolean; // handicapped accessible
 };
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+	title = 'app';
 
 	// seat sold icon
 	ssi: string = "../assets/ssi.png";
@@ -28,9 +27,19 @@ export class AppComponent implements OnInit {
 	// handicapped acessible selected
 	hss: string = "../assets/hss.png"
 
-	sections: string[] = [
-		"1",
-		"2",
+	sections: any[] = [
+		{
+			id: "1",
+			disabled: false
+		},
+		{
+			id: "2",
+			disabled: false
+		},
+		{
+			id: "3",
+			disabled: true
+		}
 	];
 
 	sel: Seat[] = [];
@@ -38,35 +47,63 @@ export class AppComponent implements OnInit {
 	seats: Seat[] = [
 		{
 			id: 1,
-			section: "1",
+			section_id: "1",
 			selected: false,
 			ha: false,
 			sold: true
 		},
 		{
 			id: 2,
-			section: "1",
+			section_id: "1",
 			selected: false,
 			sold: false,
 			ha: false
 		},
 		{
 			id: 8675309,
-			section: "2",
+			section_id: "2",
 			selected: false,
 			sold: false,
 			ha: true
 		},
 		{
 			id: 4,
-			section: "2",
+			section_id: "2",
+			selected: false,
+			sold: false,
+			ha: false
+		},
+		{
+			id: 1,
+			section_id: "3",
+			selected: false,
+			ha: false,
+			sold: true
+		},
+		{
+			id: 2,
+			section_id: "3",
+			selected: false,
+			sold: false,
+			ha: false
+		},
+		{
+			id: 8675309,
+			section_id: "3",
+			selected: false,
+			sold: false,
+			ha: true
+		},
+		{
+			id: 4,
+			section_id: "3",
 			selected: false,
 			sold: false,
 			ha: false
 		},
 	]
 
-	constructor(private fb: FormBuilder) {}
+	constructor() {}
 
 	public updatePrice(): void {
 		console.log(this.sel);
